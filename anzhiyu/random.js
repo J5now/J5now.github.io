8c2288ed024083009d3af57adbebb6e684f7d6e9
JSnow <j_snow@foxmail.com>
2025-11-18 12:36:54 +0800

    Site updated: 2025-11-18 12:36:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/17/hello-world/","2025/11/17/某战略类手游逆向分析/"];function toRandomPost(){
+var posts=["2025/11/17/某战略类手游逆向分析/","2025/11/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
