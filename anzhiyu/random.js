85bfcc439b520258be80048fe9316154a3be4837
JSnow <j_snow@foxmail.com>
2026-04-28 14:44:21 +0800

    Site updated: 2026-04-28 14:44:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/28/JDex2/","2025/11/17/世界征服者3逆向分析/","2025/11/17/飞车小游戏XPosed模块实战/","2025/11/17/hello-world/"];function toRandomPost(){
+var posts=["2026/04/28/JDex2/","2025/11/17/世界征服者3逆向分析/","2025/11/17/hello-world/","2025/11/17/飞车小游戏XPosed模块实战/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
